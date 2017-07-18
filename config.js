254dfbb38716334221a6c0b9a985158aa3581173
topherauyeung <topherauyeung@gmail.com>
2017-07-17 19:50:17 -0700

    initial bot commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key: 'blah',
-  consumer_secret: 'blah',
-  access_token: 'blah',
-  access_token_secret: 'blah'
-}
+  consumer_key: 'gSefHTBYzPKWdEP3lJ3ypLQrI',
+  consumer_secret: 'C8u2V2uehSQoCcvadc1jBQI0GwTKjQyqRHaahCmPDEYflkNDX4',
+  access_token: '887070742705168384-JHXYL2OXP1jA9TVUFkJUjE0spY6Hfl0',
+  access_token_secret: '3Kyg5sAZp4GCvM7Pr2NIBnxuOE0rOIcqgpL4LDE718qTr',
+};
